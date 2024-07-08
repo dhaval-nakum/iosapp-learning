bb704e0a8e69b7f85f543059627e2f0823ff70c5
Dhaval Nakum <dhaval.nakum@solutionanalysts.com>
2024-07-08 17:07:46 +0530

    Added Video Player

--- ioswidgetsdemo/SecondViewController.swift
+++ ioswidgetsdemo/SecondViewController.swift
@@ -6,21 +6,45 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class SecondViewController: UIViewController {
     
+    var isPlaying:Bool = false
+    var  player:AVPlayer?
+    let playIcon:String = "play.circle.fill"
+    let pauseIcon:String = "pause.circle.fill"
+    
+    @IBOutlet weak var playPauseButton: UIButton!
     
 
-    @IBOutlet weak var cwebView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        cwebView.loadRequest(URLRequest(url:  URL(string: "https://stackoverflow.com/questions/50928331/how-to-load-image-from-local-image-path-ios-swift-4")!))
 
         // Do any additional setup after loading the view.
+        let videoURL = URL(string: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_5MB.mp4")
+        player  = AVPlayer(url: videoURL!)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(playerLayer)
+        
     }
     
     
+    @IBAction func onPlayPauseClick(_ sender: Any) {
+        if(isPlaying){
+            player?.play()
+            isPlaying = false 
+            playPauseButton.setImage(UIImage(contentsOfFile: playIcon), for: .normal)
+        }else{
+            player?.pause()
+            isPlaying = true
+            playPauseButton.setImage(UIImage(contentsOfFile: pauseIcon), for: .normal)
+        }
+        
+    }
     
     /*
     // MARK: - Navigation
